1cac37fe6a0cae7171fe5926b0c43e97fb2f943c
Alexander Owen <aowen@cs.uoregon.edu>
2016-01-25 18:46:44 -0800

    Calculate opening closing times, incomplete

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ import arrow # Replacement for datetime, based on moment.js
 import datetime # But we still need time
 from dateutil import tz  # For interpreting local times
 
+# For Favicon loading
+import os
+
 # Our own module
 # import acp_limits
 
@@ -52,6 +55,10 @@ def page_not_found(error):
     flask.session['linkback'] =  flask.url_for("calc")
     return flask.render_template('page_not_found.html'), 404
 
+@app.route('/favicon.ico')
+def favicon():
+    return flask.send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 ###############
 #
@@ -62,13 +69,40 @@ def page_not_found(error):
 @app.route("/_calc_times")
 def calc_times():
   """
-  Calculates open/close times from miles, using rules 
+  Calculates open/close times from kilometers, using rules 
   described at http://www.rusa.org/octime_alg.html.
-  Expects one URL-encoded argument, the number of miles. 
+  Expects one URL-encoded argument, the number of kilometers. 
   """
-  app.logger.debug("Got a JSON request");
-  miles = request.args.get('miles', 0, type=int)
-  return jsonify(result=miles * 2)
+  #controls is of the form (control loc in km, minimum speed, maximum speed)
+  controls = [(200,15,34), (400,15,32), (600,15,30), (1000,11.428,28)] 
+  dist = request.args.get('dist', 0, type=int)
+  start_t = request.args.get('start_t', 0, type=str)
+  start_d = request.args.get('start_d', 0, type=str)
+  
+  rslt = {"opening" : "", "closing" : ""}
+  
+  
+  for control in controls:
+    km, min, max = control
+    if dist <= km:
+      time_str = "{} {}".format(start_d, start_t)
+      time = arrow.get(time_str, "MM/DD/YYYY HH:mm")
+      break
+      
+  h_min = int(dist/min)
+  m_min = round(60 * (dist/min - h_min))  
+  closing = time.replace(hours=+h_min, minutes=+m_min)
+  closing = str(closing.time())
+  
+  h_max = int(dist/max)
+  m_max = round(60 * (dist/max - h_max))  
+  opening = time.replace(hours=+h_max, minutes=+m_max)
+  opening = str(opening.time())
+  
+  rslt["closing"] = closing
+  rslt["opening"] = opening
+  
+  return jsonify(result=rslt)
  
 #################
 #
