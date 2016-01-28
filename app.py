f049591e1de15a056dcad419d3e952e601c713d8
Alexander Owen <aowen@cs.uoregon.edu>
2016-01-28 13:49:21 -0800

    update to work on ix

--- app.py
+++ app.py
@@ -149,11 +149,16 @@ def error_403(e):
 
 #############
 
-
 if __name__ == "__main__":
-    app.debug=CONFIG.DEBUG
+    # Standalone. 
+    app.debug = True
     app.logger.setLevel(logging.DEBUG)
     print("Opening for global access on port {}".format(CONFIG.PORT))
     app.run(port=CONFIG.PORT, host="0.0.0.0")
-
+else:
+    # Running from cgi-bin or from gunicorn WSGI server, 
+    # which makes the call to app.run.  Gunicorn may invoke more than
+    # one instance for concurrent service.
+    #FIXME:  Debug cgi interface 
+    app.debug=False
     
